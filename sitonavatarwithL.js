19ee2de64793d2243800018228584021298c9ba2
darbdarb <bradcooper99@gmail.com>
2023-08-19 21:32:45 +1000

    Create sitonavatarwithL.js

---
+++ sitonavatarwithL.js
@@ -0,0 +1,27 @@
+function keyPressEvent(event) {
+    if (event.text === "l" || event.text === "L") {
+        MyAvatar.beginSit(Vec3.sum(MyAvatar.position, { x: 0, y: 0.25, z: 0 }), MyAvatar.orientation);
+        var RANGE = 1;
+        var avatars = AvatarList.getAvatarsInRange(MyAvatar.position, RANGE);
+
+        if (avatars.length > 0) {
+            var closestAvatarUUID = avatars[0];
+            var inputString = closestAvatarUUID
+            var strippedString = inputString.replace(/[{}]/g, '');
+            MyAvatar.setParentID(strippedString);
+            Camera.mode = "first person";
+            print("Sitting on :" + strippedString);
+        } else {
+            print("No avatars within the specified range.");
+        }
+    } else if (event.text === "k" || event.text === "K") {
+        MyAvatar.endSit(Vec3.sum(MyAvatar.position, { x: 0, y: 0.25, z: 0 }), MyAvatar.orientation);
+        MyAvatar.setParentID(null);
+        var inputString2 = MyAvatar.sessionUUID;
+        var strippedString2 = inputString2.replace(/[{}]/g, '');
+        Camera.mode = "first person look at";
+        print("Unsitting and clearing parent UUID.");
+    }
+}
+
+Controller.keyPressEvent.connect(keyPressEvent);
