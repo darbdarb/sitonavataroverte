3fd3ff7f4bd4a816d61c5ab1f7f619400e7a193a
darbdarb <bradcooper99@gmail.com>
2023-08-20 20:13:18 +1000

    Update sitonavatarwithJ.js
    Fixed parenting loop so no more crashing

--- sitonavatarwithJ.js
+++ sitonavatarwithJ.js
@@ -1,4 +1,4 @@
-//Disable Avatar collision first in AVATAR app, click the sliders looking icon top right :)
+// Disable Avatar collision first in AVATAR app, click the sliders looking icon top right :)
 function keyPressEvent(event) {
     if (event.text === "j" || event.text === "J") {
         MyAvatar.beginSit(Vec3.sum(MyAvatar.position, { x: 0, y: 0.25, z: 0 }), MyAvatar.orientation);
@@ -9,17 +9,20 @@ function keyPressEvent(event) {
             var closestAvatarUUID = avatars[0];
             var inputString = closestAvatarUUID
             var strippedString = inputString.replace(/[{}]/g, '');
-            MyAvatar.setParentID(strippedString);
-            Camera.mode = "first person";
-            print("Sitting on :" + strippedString);
+
+            if (closestAvatarUUID !== MyAvatar.sessionUUID) {
+                MyAvatar.setParentID(strippedString);
+                Camera.mode = "first person";
+                print("Sitting on: " + strippedString);
+            } else {
+                print("Cannot sit on yourself.");
+            }
         } else {
             print("No avatars within the specified range.");
         }
     } else if (event.text === "SPACE") {
         MyAvatar.endSit(Vec3.sum(MyAvatar.position, { x: 0, y: 0.25, z: 0 }), MyAvatar.orientation);
         MyAvatar.setParentID(null);
-        var inputString2 = MyAvatar.sessionUUID;
-        var strippedString2 = inputString2.replace(/[{}]/g, '');
         Camera.mode = "first person look at";
         print("Unsitting and clearing parent UUID.");
     }
