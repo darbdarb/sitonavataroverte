e173a3a1cdde866cd5d08a4c38bd6b67b342d948
darbdarb <bradcooper99@gmail.com>
2023-08-22 00:13:53 +1000

    Update sitonavatarwithJ.js
    fixed parenting to self causing a parenting loop, thanks @ksuprynowicz

--- sitonavatarwithJ.js
+++ sitonavatarwithJ.js
@@ -1,27 +1,28 @@
 // Disable Avatar collision first in AVATAR app, click the sliders looking icon top right :)
 function keyPressEvent(event) {
     if (event.text === "j" || event.text === "J") {
-        MyAvatar.beginSit(Vec3.sum(MyAvatar.position, { x: 0, y: 0.25, z: 0 }), MyAvatar.orientation);
+        MyAvatar.beginSit(Vec3.sum(MyAvatar.position, { x: 0, y: 0.20, z: 0 }), MyAvatar.orientation);
         var RANGE = 1;
         var avatars = AvatarList.getAvatarsInRange(MyAvatar.position, RANGE);
-
+        var sessionUUID = MyAvatar.sessionUUID;
+        print(JSON.stringify(avatars));
         if (avatars.length > 0) {
-            var closestAvatarUUID = avatars[0];
-            var inputString = closestAvatarUUID
-            var strippedString = inputString.replace(/[{}]/g, '');
-
-            if (closestAvatarUUID !== MyAvatar.sessionUUID) {
-                MyAvatar.setParentID(strippedString);
-                Camera.mode = "first person";
-                print("Sitting on: " + strippedString);
-            } else {
-                print("Cannot sit on yourself.");
+            for (var i=0; i < avatars.length; i++) { 
+                var closestAvatarUUID = avatars[i];
+                // Exclude the current avatar's sessionUUID from the check
+                if (sessionUUID !== closestAvatarUUID) {
+                    MyAvatar.setParentID(sessionUUID);
+                    Camera.mode = "first person";
+                    print("Sitting on: " + strippedString);
+                } else {
+                    print("Cannot sit on yourself.");
+                }
             }
         } else {
             print("No avatars within the specified range.");
         }
     } else if (event.text === "SPACE") {
-        MyAvatar.endSit(Vec3.sum(MyAvatar.position, { x: 0, y: 0.25, z: 0 }), MyAvatar.orientation);
+        MyAvatar.endSit(Vec3.sum(MyAvatar.position, { x: 0, y: 0.20, z: 0 }), MyAvatar.orientation);
         MyAvatar.setParentID(null);
         Camera.mode = "first person look at";
         print("Unsitting and clearing parent UUID.");
